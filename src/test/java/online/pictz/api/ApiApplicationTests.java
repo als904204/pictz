d432164a29c6f5de0bb28315c0bc8de47332d70b
Min <als904204@naver.com>
2024-10-19 18:36:26 +0900

    feat:init backend

---
+++ src/test/java/online/pictz/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package online.pictz.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
