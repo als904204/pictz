9e39c49ba32a727fb8713bb8fee29a33306e7933
Min <als904204@naver.com>
2024-10-22 00:57:56 +0900

    feat: 토픽 choice 목록 조회 api 구현 #3

--- src/main/java/online/pictz/api/topic/controller/TopicApiController.java
+++ src/main/java/online/pictz/api/topic/controller/TopicApiController.java
@@ -2,6 +2,8 @@ package online.pictz.api.topic.controller;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import online.pictz.api.choice.dto.ChoiceResponse;
+import online.pictz.api.choice.service.ChoiceService;
 import online.pictz.api.topic.dto.TopicResponse;
 import online.pictz.api.topic.service.TopicService;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TopicApiController {
 
     private final TopicService topicService;
+    private final ChoiceService choiceService;
 
     @GetMapping
     public ResponseEntity<List<TopicResponse>> findAll() {
@@ -27,6 +30,12 @@ public class TopicApiController {
     public ResponseEntity<TopicResponse> findBySlug(@PathVariable String slug) {
         TopicResponse response = topicService.findBySlug(slug);
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{topicId}/choices")
+    public ResponseEntity<List<ChoiceResponse>> getChoicesForTopic(@PathVariable Long topicId) {
+        List<ChoiceResponse> choices = choiceService.getChoiceListByTopicId(topicId);
+        return ResponseEntity.ok(choices);
     }
 
 }
