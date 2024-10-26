a3428c21d913cadda2369551ee5ad34dc67900b6
Min <als904204@naver.com>
2024-10-26 16:37:14 +0900

    feat: 디바운스 함수 구현

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -7,3 +7,17 @@
 function formatNumber(num) {
     return num.toLocaleString();
 }
+
+/**
+* Debounce
+* 지정한 시간 동안 함수 이벤트가 발생하지 않으면 debounce 함수 실행
+**/
+function debounce(func, delay) {
+    let timeoutId;
+    return function(...args) {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => {
+            func.apply(this, args);
+        }, delay);
+    };
+}
