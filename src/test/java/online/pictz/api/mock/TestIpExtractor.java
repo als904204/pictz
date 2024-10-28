65792537a1599d263028a9ead26cb82f1654df62
Min <als904204@naver.com>
2024-10-28 23:04:09 +0900

    refactor: 테스트용 Mock Ip 이름 변경

--- src/test/java/online/pictz/api/mock/TestIpExtractor.java
+++ src/test/java/online/pictz/api/mock/TestIpExtractor.java
@@ -2,11 +2,11 @@ package online.pictz.api.mock;
 
 import online.pictz.api.common.util.network.IpExtractor;
 
-public class FakeIpExtractor implements IpExtractor {
+public class TestIpExtractor implements IpExtractor {
 
     private final String fixedIp;
 
-    public FakeIpExtractor(String fixedIp) {
+    public TestIpExtractor(String fixedIp) {
         this.fixedIp = fixedIp;
     }
 
