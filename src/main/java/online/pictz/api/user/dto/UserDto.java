56073eb09fee041be0b710751b21dabd4e338f99
Min <als904204@naver.com>
2024-10-31 22:48:51 +0900

    feat: 유저 dto 구현

---
+++ src/main/java/online/pictz/api/user/dto/UserDto.java
@@ -0,0 +1,11 @@
+package online.pictz.api.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserDto {
+    private final Long id;
+    private final String providerId;
+}
