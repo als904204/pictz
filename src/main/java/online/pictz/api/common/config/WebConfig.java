309c971bdc52658abbc904dafff0a3826aff91dc
Min <als904204@naver.com>
2024-11-03 18:38:12 +0900

    feat: 이미지 파일 URL 접근 가능하도록 적용

--- src/main/java/online/pictz/api/common/config/WebConfig.java
+++ src/main/java/online/pictz/api/common/config/WebConfig.java
@@ -4,13 +4,13 @@ import online.pictz.api.common.resolver.CurrentUserArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 /**
  * Spring MVC 설정을 담당하는 클래스
- * 커스텀 Argument Resolver를 등록
  */
 @Configuration
 @RequiredArgsConstructor
@@ -21,5 +21,16 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(currentUserArgumentResolver);
+    }
+
+    /**
+     * 로컬에 저장된 이미지를 /images/** 경로로 접근할 수 있도록 설정
+     *
+     * @param registry ResourceHandlerRegistry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+            .addResourceLocations("file:./uploads/");
     }
 }
