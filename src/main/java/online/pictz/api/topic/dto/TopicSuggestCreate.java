bc2f7c2bb30faac521a0d8f579b06337da635df7
Min <als904204@naver.com>
2024-11-04 17:04:02 +0900

    refactor: 토픽 문의 dto 요청, 응답 코드 리팩토링

--- src/main/java/online/pictz/api/topic/dto/TopicSuggestCreate.java
+++ src/main/java/online/pictz/api/topic/dto/TopicSuggestCreate.java
@@ -1,5 +1,6 @@
 package online.pictz.api.topic.dto;
 
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.web.multipart.MultipartFile;
@@ -10,4 +11,5 @@ public class TopicSuggestCreate {
     private String title;
     private String description;
     private MultipartFile thumbnail;
+    private List<MultipartFile> choiceImages;
 }
