badfe99f060701909d670a1e5649b33e2934e384
Min <als904204@naver.com>
2024-11-04 17:48:57 +0900

    fix: 로그인 페이지 익명 유저만 접근 가능하도록 수정

--- src/main/java/online/pictz/api/user/controller/UserPageController.java
+++ src/main/java/online/pictz/api/user/controller/UserPageController.java
@@ -1,5 +1,7 @@
 package online.pictz.api.user.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,7 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class UserPageController {
 
     @GetMapping("/login")
-    public String login() {
+    public String login(@AuthenticationPrincipal OAuth2User user) {
+        if (user != null) {
+            return "redirect:/";
+        }
         return "/login";
     }
 
