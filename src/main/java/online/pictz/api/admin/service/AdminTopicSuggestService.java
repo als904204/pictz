9b3793e071120acbbce167f93a9188fd09d16bb4
Min <als904204@naver.com>
2024-11-06 16:59:05 +0900

    feat: 어드민 토픽 문의 상태 변경 로직 구현

--- src/main/java/online/pictz/api/admin/service/AdminTopicSuggestService.java
+++ src/main/java/online/pictz/api/admin/service/AdminTopicSuggestService.java
@@ -2,10 +2,14 @@ package online.pictz.api.admin.service;
 
 import java.util.List;
 import online.pictz.api.admin.dto.AdminTopicSuggestResponse;
+import online.pictz.api.admin.dto.AdminTopicSuggestUpdateResponse;
+import online.pictz.api.topic.entity.TopicSuggestStatus;
 
 public interface AdminTopicSuggestService {
 
     List<AdminTopicSuggestResponse> getAllTopicSuggests();
 
     AdminTopicSuggestResponse getTopicById(Long id);
+
+    AdminTopicSuggestUpdateResponse patchTopicSuggestStatus(Long id, TopicSuggestStatus status, String reason);
 }
