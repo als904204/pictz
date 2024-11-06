f9703999a2a4a990917e31d7328101215cd6e7d2
Min <als904204@naver.com>
2024-11-06 16:59:58 +0900

    feat: 랜덤 slug 생성 구현

---
+++ src/main/java/online/pictz/api/topic/service/SlugGeneratorImpl.java
@@ -0,0 +1,22 @@
+package online.pictz.api.topic.service;
+
+import java.security.SecureRandom;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SlugGeneratorImpl implements SlugGenerator{
+
+    private static final String CHAR_SET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final int SLUG_LENGTH = 6;
+    private final SecureRandom random = new SecureRandom();
+
+    @Override
+    public String generate() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < SLUG_LENGTH; i++) {
+            int index = random.nextInt(CHAR_SET.length());
+            sb.append(CHAR_SET.charAt(index));
+        }
+        return sb.toString();
+    }
+}
