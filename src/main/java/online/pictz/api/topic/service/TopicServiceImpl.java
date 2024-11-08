81867580e36e5141039d1a398cad70166879c541
Min <als904204@naver.com>
2024-11-08 14:42:17 +0900

    feat: 토픽 동적 정렬 조회 비즈니스 로직 적용

--- src/main/java/online/pictz/api/topic/service/TopicServiceImpl.java
+++ src/main/java/online/pictz/api/topic/service/TopicServiceImpl.java
@@ -4,10 +4,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import online.pictz.api.common.dto.PagedResponse;
 import online.pictz.api.common.util.time.TimeProvider;
 import online.pictz.api.topic.dto.TopicResponse;
+import online.pictz.api.topic.entity.TopicSort;
 import online.pictz.api.topic.repository.TopicRepository;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -18,17 +22,27 @@ public class TopicServiceImpl implements TopicService{
     private final TimeProvider timeProvider;
 
 
-    /**
-     * 모든 토픽 조회
-     * TODO: 추후 페이징으로 변경
-     * @return 토픽 목록
-     */
+    @Deprecated
     @Override
     public List<TopicResponse> findAll() {
         return topicRepository.findActiveTopics()
             .stream()
             .map(TopicResponse::from)
             .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public PagedResponse<TopicResponse> getActiveTopics(TopicSort sortType, int page) {
+        Page<TopicResponse> topicPage = topicRepository.findActiveTopics(sortType, page);
+        return new PagedResponse<>(
+            topicPage.getContent(),
+            topicPage.getNumber(),
+            topicPage.getSize(),
+            topicPage.getTotalElements(),
+            topicPage.getTotalPages(),
+            topicPage.isLast()
+        );
     }
 
 }
