87c2d5450e1f4fb36dbb3a29eb54f23b463a9ad8
Min <als904204@naver.com>
2024-11-09 17:55:57 +0900

    feat: 유저 토픽 요청 선택지 응답 dto

---
+++ src/main/java/online/pictz/api/topic/dto/TopicSuggestChoiceImageResponse.java
@@ -0,0 +1,11 @@
+package online.pictz.api.topic.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TopicSuggestChoiceImageResponse {
+    private String imageUrl;
+    private String fileName;
+}
