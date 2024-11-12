65dcfe8687e63a5fd99945db8bfcc23d741c2238
Min <als904204@naver.com>
2024-11-12 18:01:57 +0900

    feat: 선택지 생성, 업데이트 단위 메소드로 구현

--- src/main/java/online/pictz/api/choice/entity/Choice.java
+++ src/main/java/online/pictz/api/choice/entity/Choice.java
@@ -1,5 +1,7 @@
 package online.pictz.api.choice.entity;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,6 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Version;
 import lombok.Getter;
+import online.pictz.api.topic.entity.TopicSuggestChoiceImage;
 
 @Getter
 @Entity
@@ -20,6 +23,9 @@ public class Choice {
 
     @Column(name = "topic_id", nullable = false)
     private Long topicId;
+
+    @Column(name = "suggest_choice_image_id", nullable = false)
+    private Long choiceImageId;
 
     @Column(name = "name", nullable = false)
     private String name;
@@ -37,14 +43,47 @@ public class Choice {
 
     }
 
-    public Choice(Long topicId, String name, String imageUrl) {
+    public Choice(Long topicId, String name, String imageUrl, Long choiceImageId) {
         this.topicId = topicId;
         this.name = name;
         this.imageUrl = imageUrl;
-        this.voteCount = 0;
+        this.choiceImageId = choiceImageId;
     }
 
     public void updateVoteCount(int voteCount) {
         this.voteCount += voteCount;
     }
+
+    public void updateImageDetail(String imageUrl, String fileName) {
+        this.imageUrl = imageUrl;
+        this.name = fileName;
+    }
+
+    /**
+     * topicId에 대한 새로운 선택지 생성
+     * @param topicId 선택지의 부모 토픽
+     * @param images  선택지의 이미지
+     * @return 선택지
+     */
+    public static List<Choice> createFrom(Long topicId, List<TopicSuggestChoiceImage> images) {
+        return images.stream()
+            .map(image ->
+                new Choice(topicId, image.getFileName(), image.getImageUrl(), image.getId()))
+            .collect(Collectors.toList());
+    }
+
+    /**
+     * 기존 선택지를 변경된 선택지 문의에 따라 이미지 정보 업데이트
+     * @param choices 저장되어있는 선택지 목록
+     * @param images  변경 요청온 선택지 목록 이미지 정보
+     */
+    public static void updateFrom(List<Choice> choices, List<TopicSuggestChoiceImage> images) {
+        images.forEach(image -> choices.stream()
+            .filter(choice -> choice.getId().equals(image.getId()))
+            .findFirst()
+            .ifPresent(choice -> choice.updateImageDetail(image.getImageUrl(), image.getFileName()))
+        );
+    }
+
+
 }
