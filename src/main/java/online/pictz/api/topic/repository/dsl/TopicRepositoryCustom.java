199311ef4e84aaf64d8f826f4f9e58d9fa685bcb
Min <als904204@naver.com>
2024-11-14 00:26:11 +0900

    feat: 토픽 총 투표수 조회 쿼리

--- src/main/java/online/pictz/api/topic/repository/dsl/TopicRepositoryCustom.java
+++ src/main/java/online/pictz/api/topic/repository/dsl/TopicRepositoryCustom.java
@@ -1,9 +1,14 @@
 package online.pictz.api.topic.repository.dsl;
 
+import java.util.List;
+import online.pictz.api.topic.dto.TopicCountResponse;
 import online.pictz.api.topic.dto.TopicResponse;
 import online.pictz.api.topic.entity.TopicSort;
 import org.springframework.data.domain.Page;
 
 public interface TopicRepositoryCustom {
+
     Page<TopicResponse> findActiveTopics(TopicSort sortType, int page);
+
+    List<TopicCountResponse> getTopicTotalCounts(int page);
 }
