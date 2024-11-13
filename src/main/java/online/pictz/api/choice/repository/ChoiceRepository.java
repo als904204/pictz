c55f94c58198e0de46c093539f780eb60083adcb
Min <als904204@naver.com>
2024-11-14 01:20:52 +0900

    cleanup: 필요없는 쿼리문 삭제

--- src/main/java/online/pictz/api/choice/repository/ChoiceRepository.java
+++ src/main/java/online/pictz/api/choice/repository/ChoiceRepository.java
@@ -2,13 +2,12 @@ package online.pictz.api.choice.repository;
 
 import java.util.List;
 import online.pictz.api.choice.entity.Choice;
+import online.pictz.api.choice.repository.dsl.ChoiceRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ChoiceRepository extends JpaRepository<Choice, Long> {
+public interface ChoiceRepository extends JpaRepository<Choice, Long>, ChoiceRepositoryCustom {
 
     List<Choice> findByTopicId(Long topicId);
-
-    long countById(Long id);
 
     List<Choice> findByTopicIdIn(List<Long> topicIds);
 }
