55597af6cdf1e7f5e58f7d84207405928302f429
Min <als904204@naver.com>
2024-11-17 15:42:54 +0900

    test: 투표 컨버터 단위 테스트 코드

---
+++ src/test/java/online/pictz/api/vote/service/VoteConverterTest.java
@@ -0,0 +1,84 @@
+package online.pictz.api.vote.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import online.pictz.api.mock.TestTimeProvider;
+import online.pictz.api.vote.dto.VoteRequest;
+import online.pictz.api.vote.entity.Vote;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+
+class VoteConverterTest {
+
+    private VoteConverter voteConverter;
+    private TestTimeProvider timeProvider = new TestTimeProvider(
+        LocalDateTime.of(2024, 1, 1, 1, 1));
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        this.voteConverter = new VoteConverter();
+    }
+
+    @DisplayName("투표 요청 정보에서 선택지 ID만 추출 한다")
+    @Test
+    void convertToChoiceIds() {
+
+        // given
+        Long choiceId = 1L;
+        int count = 10;
+
+        List<VoteRequest> voteRequests = List.of(new VoteRequest(choiceId, count));
+
+        // when
+        List<Long> choiceIds = voteConverter.convertToChoiceIds(voteRequests);
+
+        // then
+        assertThat(choiceIds).isNotNull();
+        assertThat(choiceIds.get(0)).isEqualTo(choiceId);
+    }
+
+    @DisplayName("투표 정보 Dto to Entity 변환")
+    @Test
+    void convertToVoteEntities() {
+
+        // given
+        Long choiceId = 1L;
+        int count = 10;
+
+        List<VoteRequest> voteRequests = List.of(new VoteRequest(choiceId, count));
+
+        String ip = "1.1.1.1";
+
+        // when
+        List<Vote> votes = voteConverter.convertToVoteEntities(voteRequests, ip,
+            timeProvider.getCurrentTime());
+
+        // then
+        assertThat(votes.get(0).getChoiceId()).isEqualTo(choiceId);
+        assertThat(votes.get(0).getCount()).isEqualTo(count);
+        assertThat(votes.get(0).getIp()).isEqualTo(ip);
+    }
+
+    @DisplayName("투표 정보 Map으로 변환")
+    @Test
+    void convertToVoteCountMap() {
+
+        // given
+        Long choiceId = 1L;
+        int count = 10;
+
+        List<VoteRequest> voteRequests = List.of(new VoteRequest(choiceId, count));
+
+        // when
+        Map<Long, Integer> resultMap = voteConverter.convertToVoteCountMap(voteRequests);
+
+        // then
+        assertThat(resultMap.get(choiceId)).isEqualTo(count);
+    }
+}
