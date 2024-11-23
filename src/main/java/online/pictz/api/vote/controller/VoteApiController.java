23b89570612fedd9ab50d217c293fe1851d011e8
Min <als904204@naver.com>
2024-11-23 17:57:29 +0900

    fix: 잘못된 api 변경 수정

--- src/main/java/online/pictz/api/vote/controller/VoteApiController.java
+++ src/main/java/online/pictz/api/vote/controller/VoteApiController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/optimistic/votes")
+@RequestMapping("/api/v1/votes")
 @RestController
 public class VoteApiController {
 
